a9211fb71d8e4592f54b0c41bcf9fcd2347fac1a
Permanaff <fajar.permanaf314@gmail.com>
2026-08-19 14:48:01 +0700

    style(eksternal): update tagline logo menjadi portal layanan dan informasi terintegrasi

--- Modules/Eksternal/resources/assets/js/components/layouts/AppShell.tsx
+++ Modules/Eksternal/resources/assets/js/components/layouts/AppShell.tsx
@@ -107,7 +107,7 @@ export const AppShell: React.FC = () => {
                 BBKKP POLIMER
               </span>
               <span className="text-[10px] text-slate-500 block uppercase tracking-wider">
-                Portal Layanan Industri
+                Portal Layanan dan Informasi Terintegrasi
               </span>
             </div>
           </Link>
